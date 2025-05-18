4cd1afe50ee1bb11689e58fb9508903d95f5ac67
Ihor Tutak <igor2tak2212@gmail.com>
2025-05-18 17:28:37 +0300

    refactor code

--- script.js
+++ script.js
@@ -46,13 +46,15 @@ const quotes = [
 ];
 
 const quoteElement = document.getElementById("quote");
+const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  const quote = randomQuote.quote + "<br>" + randomQuote.author;
-  quoteElement.innerHTML = quote;
+  const { quote, author } = randomQuote;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = author;
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
